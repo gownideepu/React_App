b4b7c98e4ba91fca91093ba7f0c1a3aa45eef455
unknown <gownideepu23@gmail.com>
2023-05-21 19:22:05 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React ,{Component} from "react";
 class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      name:"Hello from Bridgelabz"
+    }
+  }
   render(){
+    console.log("Render Method")
     return (<div>
-     Hello From Bridgelaz
+              <h1>{this.state.name}</h1>
     </div>)
   };
 
