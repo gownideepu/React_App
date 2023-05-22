1ff28a937e347a278bfc2811dbfb7bd75312e48c
unknown <gownideepu23@gmail.com>
2023-05-22 09:06:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React ,{Component} from "react";
 import logo from './logo.svg'
+import './App.css';
 class App extends Component{
+  url='https://www.bridgelabz.com';
   constructor(){
+    
     super();
     this.state={
-      userName:" "
+      userName:'',
+      nameError:''
     }
   }
-  onClickEvent(){
-    window.open("https://www.bridgelabz.com",'_blank')
+  onClick=($event)=>{
+    window.open(this.url,"blank");
   }
+  
   onInputChange=(event)=>{
-    this.setState({userName:event.target.value});
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+    this.setState({userName:event.target.value})
+    if (nameRegex.test(event.target.value)){
+      this.setState({nameError:''})
+    }else{
+      this.setState({nameError:"userName is incorrect"})
+    }
   }
   render(){
     console.log("Render Method");
     return (<div>
+      <div>
               <h1>Hello from bridgelabz {this.state.userName}</h1>
               <input type="text" onChange={this.onInputChange}/>
-              <img src={logo} alt=""onClick={this.onClickEvent}/>
-    </div>)
-  };
+              <span className="error-output">{this.state.nameError}</span>
+              <img src={logo} alt=" " onClick={this.onClick}/>
 
+        </div>
+    </div>
+    )
+    
+  }
 }
 export default App;
